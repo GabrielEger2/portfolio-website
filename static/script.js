8044d7473064db62f786d1978bacafc463b56e40
Gabriel Eger <gabriel_eger01@hotmail.com>
2023-01-11 18:08:58 -0300

    About section finished
    About section finished. Also, I`ve fixed the text on low-resolution problems and anchored the home and about sections on the mobile bar.

---
+++ static/script.js
@@ -0,0 +1,11 @@
+/*Lateral Menu open and close function*/
+
+const menuMobile = document.querySelector(".menu-mobile");
+const body = document.querySelector("body");
+
+menuMobile.addEventListener("click", () => {
+  menuMobile.classList.contains("bi-list")
+    ? menuMobile.classList.replace("bi-list", "bi-x")
+    : menuMobile.classList.replace("bi-x", "bi-list");
+  body.classList.toggle("menu-nav-active");
+});
