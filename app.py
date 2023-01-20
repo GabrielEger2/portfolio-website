e66778cbc5db1fedea76566cb1a0c1a9d943f9f7
Gabriel Eger <52424334+GabrielEger2@users.noreply.github.com>
2023-01-20 19:14:35 -0300

    configured the email form and deployed to Heroku
    I've finally configured the email form with .env files for better security. Additionally, I've deployed the application using git and Heroku.

--- app.py
+++ app.py
@@ -1,11 +1,56 @@
-from flask import Flask, render_template, redirect
+from flask import Flask, render_template, redirect, request, flash
 from flask_mail import Mail, Message
+from dotenv import load_dotenv
+import os
+load_dotenv()
 
 app = Flask(__name__)
+app.secret_key = os.getenv("KEY")
+
+mail_settings = {
+    "MAIL_SERVER": 'sntp.gmail.com',
+    "MAIL_PORT": 465,
+    "MAIL_USE_TLS": False,
+    "MAIL_USE_SSL": True,
+    "MAIL_USERNAME": os.getenv("EMAI"),
+    "MAIL_PASSWORD": os.getenv("PASSWORD")
+}
+
+app.config.update(mail_settings)
+mail = Mail(app)
+
+class Contact:
+    def __init__(self, name, email, message):
+        self.name = name
+        self.email = email
+        self.message = message
 
 @app.route('/')
 def index():
     return render_template('index.html')
+
+@app.route('/send', methods=['GET', 'POST'])
+def send():
+    if request.method == 'POST':
+        form_contact = Contact(
+            request.form["name"],
+            request.form["email"],
+            request.form["message"]
+        )
+
+        msg = Message(
+            subject = f'{form_contact.name} has send you a message',
+            sender = app.config.get("MAIL_USERNAME"),
+            recipients= ['gabriel_eger01@hotmail.com', app.config.get("MAIL_USERNAME")],
+            body = f'''
+            
+            {form_contact.name} with the e-mail {form_contact.email}, has send you the following message:
+            {form_contact.message}
+            '''
+        )
+        mail.send(msg)
+        flash('Thank you. Your email has been sent!')
+    return redirect('/')
 
 if __name__ == '__main__':
     app.run(debug=True)
--- static/script.js
+++ static/script.js
@@ -2,7 +2,9 @@
 const menuMobile = document.querySelector(".menu-mobile");
 const body = document.querySelector("body");
 menuMobile.addEventListener("click", () => {
-  menuMobile.classList.contains("bi-list")
+  menuMobile.classList.contains("bi-list-ul")
+    ? menuMobile.classList.replace("bi-list-ul", "bi-x")
+    : menuMobile.classList.replace("bi-x", "bi-list-ul");
   body.classList.toggle("menu-nav-active");
 });
 
@@ -15,6 +17,22 @@ navItem.forEach(item => {
   item.addEventListener("click", () => {
     if (body.classList.contains("menu-nav-active")) {
       body.classList.remove("menu-nav-active")
+      menuMobile.classList.replace("bi-x", "bi-list-ul");
     }
   })
 })
+
+
+/* Loading and notification for email*/
+
+const btnSend = document.querySelector('#btn-send')
+const btnSendLoader = document.querySelector('#btn-send-loader')
+
+btnSend.addEventListener("click", ()=>{
+  btnSendLoader.style.display = "block";
+  btnSend.style.display = "none"
+})
+
+setTimeout(() => {
+  document.querySelector('#alert').style.display = 'none';
+}, 5000)
